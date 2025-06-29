282cd3ba44953f922325e5bb5325d5c23ff1b533
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-29 18:40:43 -0400

    style: Extract styles from HomeScreen.tsx into a dedicated file
    Move component-specific styles into a separate file called homeStyles.js and adding the import in HomeScreen.tsx

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,5 +1,6 @@
-import { FlatList, Text, View, Image, StyleSheet} from "react-native";
+import { FlatList, Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import homeStyles from './homeStyles'
 
 
 export default function HomeScreen() {
@@ -10,37 +11,23 @@ export default function HomeScreen() {
     }
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#000000'}}>
-            <View style={styles.container}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#000000' }}>
+            <View style={homeStyles.headerContent}>
                 <Image
                     source={{ uri: 'https://cdn.royaleapi.com/static/img/branding/royaleapi-logo-128.png?t=feb800c3c' }}
-                    style={styles.logo}
+                    style={homeStyles.logo}
                 />
-                <Text style={styles.text}>RoyaleAPI Mobile</Text>
+                <Text style={homeStyles.text}>RoyaleAPI Mobile</Text>
+
             </View>
-            
+
             <FlatList
                 data={lista}
                 keyExtractor={(item) => item}
                 renderItem={({ item }) => (
-                    <Text style={{color: 'white'}}>{item}</Text>
+                    <Text style={{ color: 'white' }}>{item}</Text>
                 )}
             />
         </SafeAreaView>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    logo: {
-        height: 32,
-        width: 32,
-    },
-    text: {
-        paddingLeft: 10,
-        color: 'white'
-    },
-});

c71cf8929e0608e205c2a21b879c06b1ddb73292
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-03 23:44:16 -0400

    feat: Add endpoint for fetching Player by playerTag

--- src/services/network/ClashAPI.ts
+++ src/services/network/ClashAPI.ts
@@ -1,8 +1,15 @@
 import { ApiService } from './ApiService';
 import { CardsResponse } from '../../models/Card';
+import { Player } from '@/src/models/Player';
 
 export class ClashRoyaleAPI extends ApiService {
+  //cards endpoints
   async getCards(): Promise<CardsResponse> {
     return this.get<CardsResponse>('/cards');
   }
+
+  //players endpoints
+  async getPlayer(tag: string): Promise<Player> {
+    return this.get<Player>(`players/${tag}`)
+  } 
 }

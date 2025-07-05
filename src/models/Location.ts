fac9a74f6c0046158a8e55f94c438bd586bbc4cf
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-05 19:01:39 -0400

    feat: Created a Model for Location

---
+++ src/models/Location.ts
@@ -0,0 +1,13 @@
+export interface Location {
+    id: number,
+    name: string,
+    isCountry: boolean;
+    countryCode?: string;
+    localizedName?:string;
+    flagUrl?: string;
+}
+
+// /locations
+export interface Locations {
+    items: Location[]
+}

8643f81fe837bc2b78dd06f81a04a31ef92009da
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-06 14:40:20 -0400

    feat: Add button navigation section and visual welcome to HomeScreen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,27 +1,59 @@
-import { FlatList, Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import homeStyles from './homeStyles'
+import { RootDrawerParamList } from "@/src/navigation/navigation";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { useNavigation } from "@react-navigation/native";
 
+type HomeNavigationProp = DrawerNavigationProp<RootDrawerParamList>;
 
 export default function HomeScreen() {
-    let lista: string[] = []
-    for (let index = 0; index < 100; index++) {
-        const element = (index + 1) + "";
-        lista.push(element);
-    }
+    const navigation = useNavigation<HomeNavigationProp>();
 
     return (
-        <SafeAreaView 
+        <SafeAreaView
             style={{ flex: 1, backgroundColor: '#000000' }}
             edges={['bottom', 'left', 'right']}
         >
-            <FlatList
-                data={lista}
-                keyExtractor={(item) => item}
-                renderItem={({ item }) => (
-                    <Text style={{ color: 'white' }}>{item}</Text>
-                )}
-            />
+            <View style={homeStyles.container}>
+                <View style={homeStyles.initSection}>
+                    <Image source={require('../../assets/images/King.png')} style={homeStyles.initSectionImage} />
+                    <Text style={homeStyles.initSectionTitle}>!BIENVENIDO A INFO ROYALE¡</Text>
+                    <Text style={[homeStyles.initSectionText, {textAlign: 'center'}]}>Aqui podras buscar informacion relevante de Clash Royale</Text>
+                </View>
+                <View style={homeStyles.section}>
+                    <Text style={homeStyles.sectionTitle}>Ver Informacion sobre:</Text>
+                    <View style={homeStyles.gridContainer}>
+                        <View style={homeStyles.gridRow}>
+                            <TouchableOpacity
+                                style={[homeStyles.gridItem, { marginRight: 5 }]}
+                                onPress={() => navigation.navigate('Player', { tag: '' })}
+                            >
+                                <Image source={require('../../assets/images/kingBlue.png')} style={homeStyles.gridImage} />
+                                <Text style={homeStyles.gridTitle}>Players</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                                style={[homeStyles.gridItem, { marginLeft: 5 }]}
+                                onPress={() => navigation.navigate('Clans', { tag: '' })}
+                            >
+                                <Image source={require('../../assets/images/social.webp')} style={homeStyles.gridImage} />
+                                <Text style={homeStyles.gridTitle}>Clans</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={homeStyles.gridRow}>
+                            <TouchableOpacity
+                                style={homeStyles.gridItem}
+                                onPress={() => navigation.navigate('Cards')}
+                            >
+                                <Image source={require('../../assets/images/donatedcards.webp')} style={homeStyles.gridImage} />
+                                <Text style={homeStyles.gridTitle}>Cards</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
--- src/screens/home/homeStyles.js
+++ src/screens/home/homeStyles.js
@@ -1,6 +1,75 @@
 import { StyleSheet } from "react-native";
 
 const homeStyles = StyleSheet.create({
+    container: {
+        padding: 10,
+        flex: 1,
+    },
+    initSection: {
+        alignSelf: 'flex-start',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#2370b8',
+        borderRadius: 8,
+        width: '100%',
+        marginBottom: 10,
+    },
+    initSectionTitle: {
+        padding: 10,
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    initSectionText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    initSectionImage: {
+        width: 170,
+        height: undefined,
+        aspectRatio: 70/90,
+        marginRight: 10,
+    },
+    section: {
+        alignSelf: 'flex-start',
+        padding: 10,
+        backgroundColor: '#444',
+        borderRadius: 8,
+        width: '100%',
+    },
+    sectionTitle: {
+        padding: 10,
+        color: '#BB86FC',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    gridContainer: {
+        padding: 10,
+    },
+    gridRow: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    gridItem: {
+        flex: 1,
+        backgroundColor: '#666',
+        borderRadius: 8,
+        padding: 10,
+        height: 80,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    gridImage: {
+        width: 40,
+        height: 40,
+        marginRight: 10,
+    },
+    gridTitle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
 });
 
 export default homeStyles;

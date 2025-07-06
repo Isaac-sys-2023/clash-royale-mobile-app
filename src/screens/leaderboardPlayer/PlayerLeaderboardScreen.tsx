04a1cf8535f1aa702c16b5db1866b1d054f3da87
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-06 19:10:38 -0400

    feat: Created Player Leaderboard Screen and his styles

--- src/screens/leaderboardPlayer/PlayerLeaderboardScreen.tsx
+++ src/screens/leaderboardPlayer/PlayerLeaderboardScreen.tsx
@@ -1,13 +1,25 @@
-import { Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import playerLeaderboardStyles from './playerLeaderboardStyles'
+import LeaderboardPlayers from "@/src/components/leaderboard/players/LeaderboardPlayers";
 
-export default function DecksScreen() {
+export default function PlayerLeaderboardScreen() {
     return (
-        <SafeAreaView 
+        <SafeAreaView
             style={{ flex: 1, backgroundColor: '#000' }}
             edges={['bottom', 'left', 'right']}
         >
-            <Text style={{color:'white'}}>Decks Screen</Text>
+            <StatusBar style="light" />
+
+            <ScrollView
+                style={playerLeaderboardStyles.container}
+                contentContainerStyle={playerLeaderboardStyles.contentContainer}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+            >
+                <LeaderboardPlayers title="Top 50 Players in your country" limit={50}/>
+            </ScrollView>
         </SafeAreaView>
 
     )
---
+++ src/screens/leaderboardPlayer/playerLeaderboardStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const playerLeaderboardStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1A1A1A',
+    },
+    scrollContainer: {
+        flex: 1,
+    },
+    contentContainer: {
+        padding: 16,
+    },
+})
+
+export default playerLeaderboardStyles;
